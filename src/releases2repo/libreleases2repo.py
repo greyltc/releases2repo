f416c8470570836b6780b0f922782b65e90e3dc7
M. Greyson Christoforo <grey@christoforo.net>
2026-04-30 16:46:46 -0600

    add error message about caddy-api

--- src/releases2repo/libreleases2repo.py
+++ src/releases2repo/libreleases2repo.py
@@ -359,6 +359,10 @@ class Releases2Repo:
                 print(
                     f"Failed to query Caddy root config: {e.code} {e.reason}: {err_body}"
                 )
+                if e.code == 111:
+                    print(f"Is caddy's api server running on {base.rstrip('/config')}? Hint:")
+                    print("systemctl start caddy-api")
+                    sys.exit(1)
                 return False
             except json.JSONDecodeError:
                 pass
